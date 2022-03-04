cb0290034316067ad3b447da0a8545e4920cf8e2
daniel.alarcon <daniel.alarcon@iotsens.com>
2022-03-04 12:50:03 +0100

    Cambios

--- controllers.js
+++ controllers.js
@@ -13,20 +13,23 @@ sse.start()
 const schema = buildSchema(`
   type Query {
     users: [User]
-    searchUser(blogId:ID!, q:String!):[User]
     events: [Event]
-    searchEvent(q:String!):[Event]
-    searchUsersInEvent(q:String!):[users]
-    searchEventsInUsers(q:String!):[events]
+    searchUser(idUser:ID!):[User]
+    searchEvent(idEvent:ID!):[Event]
+    searchUsersInEvent(idEvent:ID!):[users]
+    searchEventsInUser(idUser:ID!):[events]
   }
+  
   type Mutation {
     addUser(identifier:ID!,name:String!,familyName:String!,birthDate:Int!,gender:String!,preferences:String!):User!
     addEvent(identifier:ID!,about:String!,startDate:Int!,location:String!,duration:Int!):Event!
   }
+  
   type User{
     identifier: ID!
-	  name: String
+	name: String
     familyName: String
+    performerIn: [Event]
     birthDate: Int
     gender: String
     preferences: [String]
@@ -34,49 +37,69 @@ const schema = buildSchema(`
 
   type Event{
     identifier: ID!
-	  about: String
+	about: String
     attendee: [User]
     startDate: Int
-	  location: String
-	  duration: Int
+	location: String
+	duration: Int
   }
 `)
 
 
 const rootValue = {
      users : () => DB.objects('User'),
-     events:  () => DB.objects('Event'),
-     searchEvent: ({ q }) => {
-       q = q.toLowerCase()
-       return DB.objects('Event').filter(x => x.title.toLowerCase().includes(q))
+     events : () => DB.objects('Event'),
+     searchEvent: ({ idEvent }) => {
+       return DB.objects('Event').filter(x => x.identifier.includes(idEvent))
      },
-     posts: ({ blogId }) => {
-       return DB.objects('Post').filter(x => x.blog.title == blogId)
-     },
-     addPost: ({title, content, authorId, blogId}) => {
+    searchUser: ({ idUser }) => {
+        return DB.objects('User').filter(x => x.identifier.includes(idUser))
+    },
+    searchUsersInEvent: ({ idEvent }) => {
+        return  DB.objects('Event').filter(x => x.identifier.includes(idEvent)).attendee
+    },
+    searchEventsInUser: ({ idUser }) => {
+        return  DB.objects('User').filter(x => x.identifier.includes(idUser)).performerIn
+    },
+    addUser: ({identifier, name, familyName, birthDate, gender, preferences}) => {
 
-       let blog = DB.objectForPrimaryKey('Blog', blogId)
-       let auth = DB.objectForPrimaryKey('User', authorId)
-       let data = null
-       
-       if (blog && auth){
-          data = {
-                       title: title,
-                       content: content,
-                       author: auth,
-                       blog: blog,
-                       timestamp: new Date()
-                      }
+        let user = {
+            identifier: identifier,
+            name: name,
+            familyName: familyName,
+            birthDate: birthDate,
+            gender: gender,
+            preferences: preferences,
+            performerIn: []
+        }
 
-          DB.write( () => { DB.create('Post', data) }) 
+        DB.write( () => { DB.create('User', user) })
 
-          // SSE notification (same view as in graphQL)
-          let post = {title: data.title, content: data.content, author: {name: data.author.name}, blog: {title: blog.title}}
-          sse.emitter.emit('new-post', post)
-       }
+        // SSE notification (same view as in graphQL)
+        let post = {identifier: user.identifier, name: user.name, familyName: user.familyName, birthDate: user.birthDate, gender: user.gender, preferences: user.preferences}
+        sse.emitter.emit('new-post', post)
+        return user
+    },
 
-       return data
-     }
+    addEvent: ({identifier, about, startDate, location, duration}) => {
+
+        let event = {
+            identifier: identifier,
+            about: about,
+            attendee: [],
+            startDate: startDate,
+            location: location,
+            duration: duration,
+
+        }
+
+        DB.write( () => { DB.create('Event', event) })
+
+        // SSE notification (same view as in graphQL)
+        let post = {identifier: event.identifier, about: event.about, startDate: event.startDate, location: event.location, duration: event.duration}
+        sse.emitter.emit('new-post', post)
+        return event
+    }
 }
 
 exports.root   = rootValue
--- model.js
+++ model.js
@@ -9,7 +9,8 @@ let UserSchema = {
       familyName: 'string',
       birthDate: 'date',
       gender: 'string',
-      preferences: '[string]'
+      preferences: '[string]',
+      performerIn: '[Event]'
    }
 }
 
@@ -19,7 +20,7 @@ let EventSchema = {
   properties: {
     identifier: 'int',
     about: 'string',
-    atendee: '[User]', 
+    attendee: '[User]',
     startDate: 'date',
     location: 'string',
     duration: 'int'
@@ -28,7 +29,7 @@ let EventSchema = {
 
 // // // MODULE EXPORTS
 
-let config = {path: './data/blogs.realm', schema: [PostSchema, UserSchema, BlogSchema]}
+let config = {path: './data/blogs.realm', schema: [UserSchema, EventSchema]}
 
 exports.getDB = async () => await Realm.open(config)
 
@@ -42,34 +43,42 @@ if (process.argv[1] == __filename){ //TESTING PART
 
       let DB = new Realm({
         path: './data/blogs.realm',
-        schema: [PostSchema, UserSchema, BlogSchema]
+        schema: [UserSchema, EventSchema]
       })
      
       DB.write(() => {
-        let user = DB.create('User', {name:'user0', passwd:'xxx'})
-        
-        let blog = DB.create('Blog', {title:'Todo Motos', creator: user})
-        
-        let post = DB.create('Post', {
-                                        title: 'prueba moto', 
-                                        blog: blog, 
-                                        content: 'esto es una prueba de motos',
-                                        creator: user, 
-                                        timestamp: new Date()})
+        let user = DB.create('User', {
+            identifier: 0,
+            name: 'Usuario0',
+            faimilyName: 'Apellido',
+            performerIn: {},
+            birthDate: 20220304,
+            gender: 'Hombre',
+            preferences: ['Fiesta','Cine']
+        })
 
-        console.log('Inserted objects', user, blog, post)
+        let event = DB.create('Event', {
+                                        identifier: 0,
+                                        about: 'Cumpleaños',
+                                        attendee: [],
+                                        startDate: 20220404,
+                                        location: 'Castellon',
+                                        duration: '30'
+        })
+
+        console.log('Inserted objects', user, event)
       })
       DB.close()
 
   }
   else { //consultar la BD
 
-      Realm.open({ path: './data/blogs.realm' , schema: [PostSchema, UserSchema, BlogSchema] }).then(DB => {
+      Realm.open({ path: './data/blogs.realm' , schema: [UserSchema, EventSchema] }).then(DB => {
         let users = DB.objects('User')
         users.forEach(x => console.log(x.name))
-        let blog = DB.objectForPrimaryKey('Blog', 'Todo Motos')
-        if (blog)
-           console.log(blog.title, 'by', blog.creator.name)
+        let event = DB.objectForPrimaryKey('Event', '0')
+        if (event)
+           console.log(event.about, ' in ', event.location)
         DB.close()
       })
   }
